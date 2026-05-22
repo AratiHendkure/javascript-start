7fa7ee2a964c4195df206d98486bfc599ae086bd
Arati Hendkure <aratihendkure60@gmail.com>
2026-05-22 08:54:17 +0000

    data types

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer js 
+// alert(3+4) // we are using node js not browsers
+
+let name = "Arati"
+let age = 22
+let isLoggedIn = false
+let state; // undefined , abhi value asign nahi hui
+let State = null // null value 
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => Standalone value, representaion of emty value
+// undefined // 
+// symbol => unique 
+
+
+// Object 
+
+console.log(typeof null) // object 
+console.log(typeof undefined) // undefined
+
