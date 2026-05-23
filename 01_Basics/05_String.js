04dc651720237613a9ac2501dce0b4e9d954b8b3
Arati Hendkure <aratihendkure60@gmail.com>
2026-05-23 16:14:53 +0000

    string

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,22 @@
+const name = "Arati"
+const repoCount = 50
+
+//console.log(name + repoCount+"Value");
+
+console.log(`hellow thsi is ${name} and this is my repocount ${repoCount}`);
+
+const gameName = new String('Arati-hendkure')
+console.log(gameName[0])
+console.log(gameName.__proto__) // can get prototypes ex gameName.lenght
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('t'))
+
+const nweString = gameName.substring(0,4)
+console.log(nweString)
+
+const anotherString 
+
+
