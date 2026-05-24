2de2c3305d0670144f9fc7d6affe9fb20ecf28c4
Arati Hendkure <aratihendkure60@gmail.com>
2026-05-24 13:24:57 +0000

    learn about random()

--- 01_Basics/06_numberandMaths.js
+++ 01_Basics/06_numberandMaths.js
@@ -34,4 +34,5 @@ console.log(Math.floor(Math.random()*10) + 1);
 const min = 1
 const max = 6
 
-console.log(Math.floor((Math.random()* ((max-min) + 1))) + min) //
+console.log(Math.floor((Math.random()* ((max-min) + 1))+ min)) 
+console.log(Math.floor(4.9))
