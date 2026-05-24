f2981f2dcd60902fc84a465b9190d8b7649415db
Arati Hendkure <aratihendkure60@gmail.com>
2026-05-24 10:43:19 +0000

    Math and num

---
+++ 01_Basics/06_numberandMaths.js
@@ -0,0 +1,37 @@
+ const score = 400
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// const otherNumber = 123.9032
+
+// console.log(otherNumber.toPrecision(4))
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString()) // american Standard
+// console.log(hundred.toLocaleString('en-IN'))
+
+// //++++++++++++++++ Maths +++++++++++++
+
+// console.log(Math) // write this on console where there can see the all mathematical operations
+
+// console.log(Math.abs(-4)); // negative value become positive thats called absolute
+
+// console.log(Math.round(4.6)); // roundoff the value
+
+// console.log(Math.ceil(6.5)); // which chooses top value answer is 7
+
+// console.log(Math.floor(6.5)); // which select lowest value answer is 6
+
+// console.log(Math.min(6, 6, 7, 9, 2)); // minimum value select
+
+// console.log(Math.max(6, 3, 4, 2));
+
+console.log(Math.random()) // come value between 0 to 1
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random()* ((max-min) + 1))) + min) //
