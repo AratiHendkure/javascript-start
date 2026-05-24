26b052178484f35a845c36d59e7078d59c3326fd
Arati Hendkure <aratihendkure60@gmail.com>
2026-05-24 09:26:57 +0000

    learn some string operations

--- 01_Basics/05_String.js
+++ 01_Basics/05_String.js
@@ -1,22 +1,43 @@
-const name = "Arati"
-const repoCount = 50
+// const name = "Arati"
+// const repoCount = 50
 
-//console.log(name + repoCount+"Value");
+// //console.log(name + repoCount+"Value");
 
-console.log(`hellow thsi is ${name} and this is my repocount ${repoCount}`);
+// console.log(`hellow thsi is ${name} and this is my repocount ${repoCount}`);
 
-const gameName = new String('Arati-hendkure')
-console.log(gameName[0])
-console.log(gameName.__proto__) // can get prototypes ex gameName.lenght
+// const gameName = new String('Arati-hendkure')
+// console.log(gameName[0])
+// console.log(gameName.__proto__) // can get prototypes ex gameName.lenght
 
-console.log(gameName.length)
-console.log(gameName.toUpperCase())
-console.log(gameName.charAt(2))
-console.log(gameName.indexOf('t'))
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+// console.log(gameName.anchor(" "))
 
-const nweString = gameName.substring(0,4)
-console.log(nweString)
+// const nweString = gameName.substring(0,4)
+// console.log(nweString)
 
-const anotherString 
+const name = new Number(2003)
+const name2 = name.toString()
+
+console.log(name2)
+
+console.log(typeof name2.valueOf())
+
+// console.log(typeof name)
+
+// const newNumberName = name.valueOf()
+// console.log(newNumberName)
+// console.log( typeof newNumberName)
+
+const stringName = new String("Arati Hendkure")
+console.log(stringName)
+
+// const newStringName = stringName.valueOf()
+// console.log(newStringName)
+// console.log( typeof newStringName)
+
+
 
 
